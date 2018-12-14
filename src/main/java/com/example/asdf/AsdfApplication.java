8ad115fd64472e31c37e2ca882ac37fdfa4993f0
olga_dmitrova@epam.com <Olga_Dmitrova@epam.com>
2018-12-14 14:03:40 +0300

    first commit

---
+++ src/main/java/com/example/asdf/AsdfApplication.java
@@ -0,0 +1,14 @@
+package com.example.asdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsdfApplication.class, args);
+	}
+
+}
+
